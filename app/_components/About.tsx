95cd833bb3dd49061a181832fa8c1c8c0a518d3e
Mahmoud Taghinia <mahmoud.taghinia@outlook.com>
2024-02-11 15:47:30 +0000

    Changed about section text

--- app/_components/About.tsx
+++ app/_components/About.tsx
@@ -4,14 +4,18 @@ export default function About() {
       <div className="flex w-1/2 flex-col gap-y-4">
         <h2 className="text-4xl font-bold">About me</h2>
         <div>
-          I am a freelance front-end developer who is passionate about coding
-          and always eager to learn new technologies and tools that can enhance
-          my skills and improve my workflow. I have more than four years of
-          experience in creating responsive and user-friendly websites using
-          renowned and widely used frameworks in the industry, React and
-          Next.js. I enjoy working on challenging projects and collaborating
-          with other developers to deliver high-quality solutions that meet the
-          client's needs and expectations
+          A detail-oriented web developer with 4+ years of experience in
+          creating responsive and user-friendly websites, web apps, admin panels
+          and dashboards spanning across diverse industries including
+          cryptocurrency, betting, shopping, and farm management using renowned
+          and widely used frameworks in the industry, React and Next.js.
+          Possesses excellent communication and problem-solving skills, able to
+          build robust working relationships and deliver high-quality solutions
+          with precision and attention to detail. Proven track record of
+          applying best SEsO techniques and performance optimization resulting
+          in better discoverability and user experience. Eager to learn
+          cutting-edge technologies and tools that improve workflow and enjoy
+          challenges that lead to enhanced skillset.
         </div>
       </div>
       <div className="rounded-4xl flex h-96 w-1/2 flex-col items-center gap-y-4 border border-white px-20">
